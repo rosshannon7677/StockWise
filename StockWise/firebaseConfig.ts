501454565c3395f7e79be5a177b12d65c262848e
Ross Hannon <G00381859@gmit.ie>
2024-11-01 16:52:29 +0000

    Import added to firebaseConfig and added protected routes with react router v5 and set up authentication redirection

--- StockWise/firebaseConfig.ts
+++ StockWise/firebaseConfig.ts
@@ -1,12 +1,14 @@
 // src/firebaseConfig.ts
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBYat8b41fkc15eIdoLSywsvgWIjJLBKdA",
   authDomain: "stockwise-8351f.firebaseapp.com",
   projectId: "stockwise-8351f",
-  storageBucket: "stockwise-8351f.firebasestorage.app",
+  storageBucket: "stockwise-8351f.appspot.com",
   messagingSenderId: "151501605989",
   appId: "1:151501605989:web:181214a27c061355678a8c",
   measurementId: "G-4951NEZY9V"
@@ -15,3 +17,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
--- StockWise/src/App.tsx
+++ StockWise/src/App.tsx
@@ -1,31 +1,11 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
+import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import { Route, Redirect, Switch } from "react-router-dom";
 
-import Login from './components/Login';
-import Signup from './components/Signup';
-
-import Home from './pages/Home';
-
-
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-
-/* Theme variables */
-import './theme/variables.css';
+import Home from "./pages/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import ProtectedRoute from "./ProtectedRoute"; // Import the ProtectedRoute component
 
 setupIonicReact();
 
@@ -33,18 +13,12 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/signup">
-          <Signup />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
+        <Switch>
+          <ProtectedRoute path="/home" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Redirect exact from="/" to="/home" />
+        </Switch>
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
