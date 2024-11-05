5e8935605caaf3ad4bb42fa54c0bcb2b902a38f9
Ross Hannon <G00381859@gmit.ie>
2024-11-05 17:11:00 +0000

    AddItem called in the home page for testing

--- StockWise/src/pages/Home.tsx
+++ StockWise/src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { IonPage, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
+import AddItem from '../components/AddItem';
+
 
 const Home: React.FC = () => {
   return (
@@ -13,6 +15,11 @@ const Home: React.FC = () => {
       <IonContent className="home-content">
         <h2 className="home-title">Hello, User!</h2>
         <p className="home-subtext">Your inventory management starts here.</p>
+        <div>
+      <h1>Inventory Management System</h1>
+      <AddItem />
+      {/* Additional home page content can go here */}
+    </div>
       </IonContent>
     </IonPage>
   );
