010a0789065b83e8c7d03262c9347bf1027e3530
Ross Hannon <G00381859@gmit.ie>
2024-11-05 16:59:26 +0000

    Files for creating items started

---
+++ StockWise/src/components/AddItem.tsx
@@ -0,0 +1,13 @@
+// src/components/AddItem.tsx
+import React, { useState } from "react";
+
+const AddItem: React.FC = () => {
+ 
+  return (
+    <form >
+      
+    </form>
+  );
+};
+
+export default AddItem;
