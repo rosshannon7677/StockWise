d25dd6087c31a595a38764c81ea87ed50f387ea9
Ross Hannon <G00381859@gmit.ie>
2024-11-05 17:02:48 +0000

    adding a handler for the the items that are going to be added

--- StockWise/src/components/AddItem.tsx
+++ StockWise/src/components/AddItem.tsx
@@ -2,10 +2,27 @@
 import React, { useState } from "react";
 
 const AddItem: React.FC = () => {
- 
+  
+  const handleSubmit = async (e: React.FormEvent) => {
+   
+  };
+
   return (
-    <form >
-      
+    <form onSubmit={handleSubmit}>
+      <h3>Add New Inventory Item</h3>
+      <label>
+        Name:
+      </label>
+      <label>
+        Quantity:        
+      </label>
+      <label>
+        Price:   
+      </label>
+      <label>
+        Description:       
+      </label>
+      <button type="submit">Add Item</button>
     </form>
   );
 };
