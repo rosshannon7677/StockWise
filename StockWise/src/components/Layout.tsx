8572324e74b82e52bc7b6c6ee18673d20bb808a4
Ross Hannon <G00381859@gmit.ie>
2024-11-21 22:13:11 +0000

    Creaated a layout component with Ionic imports and location hook

---
+++ StockWise/src/components/Layout.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  IonPage,
+  IonSplitPane,
+  IonMenu,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonIcon,
+  IonLabel,
+  IonMenuToggle,
+} from '@ionic/react';
+import { homeOutline, listOutline, statsChartOutline, settingsOutline } from 'ionicons/icons';
+import { useLocation } from 'react-router';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: 
+
+export default Layout;
